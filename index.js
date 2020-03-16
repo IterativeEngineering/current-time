6e964edb73b5e1164ac625622352251c616077ea
adam-drozdz-ie <56605753+adam-drozdz-ie@users.noreply.github.com>
2020-03-16 19:28:06 +0100

    use timestamp instead of ISO string

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  const time = new Date().toISOString();
-  core.setOutput("time", time);
+  const time = +(new Date());
+  core.setOutput("time", String(time));
 } catch (error) {
   core.setFailed(error.message);
 }
